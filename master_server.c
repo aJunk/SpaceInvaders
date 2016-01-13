03d54906c6d019b814412a1e02abf67e86c377d5
Philipp Gotzmann <philipp.gotzmann@hotmail.com>
2016-01-13 14:25:24 +0100

    master_server test working

--- master_server.c
+++ master_server.c
@@ -1,4 +1,4 @@
-/* Siple program forking itselfe whenever SPACE is pressed and printing all active children to the screen.
+/* Siple program forking itselfe whenever "p" is pressed and printing all active children to the screen.
  * Children die autmatically after 30 seconds.
  */
 #define _BSD_SOURCE
@@ -26,42 +26,52 @@ int main(int argc, char *argv[])
 
     while(1){
       scanf("%c",&c);
-      if (c == ' ' && numchild < MAXCHILD) {
+      if ((c == 'p') && (numchild >= MAXCHILD)) {
+          printf("maximal number of children reached\n");
+      }
+      else if(c == 'p'){
           cpid = fork();
           c='X';
       }
-      if (cpid == -1) {    // fork failed
+      else {
+          continue;
+      }
+
+      if (cpid == -1) {       // fork failed
           perror("fork");
           exit(EXIT_FAILURE);
       }
       else if (cpid != 0) {    // I am a parent
           numchild ++;
-          for(i=0; i<MAXCHILD || memcpid[i]==0; i++); //go to next empty place
+          for(i=0; (i<MAXCHILD) && (memcpid[i]!=0); i++); //go to next empty place
           if (i<MAXCHILD) {  //if not full
               memcpid[i]=cpid;
           }
           else {
-            printf("maximal number of children reached\n");
+            printf("Error: reached end of array");
+            exit(EXIT_FAILURE);
           }
 
           printf("currently running processes:\n"); //check for active children
           for(i=0; i<MAXCHILD; i++){
-              result = waitpid(memcpid[i], &status, WNOHANG); //return imediately if process is still alive
-              if (result == 0) {
-                    printf("%d   PID:%d\n",i+1,memcpid[i]); // Child still alive
-              } else if (result == -1) {
-                    perror("waitrpid");  //Error -> Exit?
-              } else {
-                    numchild --;       // Child exited
-                    memcpid[i]=0;
+              if(memcpid[i]!=0){
+                  result = waitpid(memcpid[i], &status, WNOHANG); //return imediately if process is still alive
+                  if (result == 0) {
+                        printf("%d   PID:%d\n",i+1,memcpid[i]); // Child still alive
+                  } else if (result == -1) {
+                        perror("waitpid");  //Error -> Exit?
+                  } else {
+                        numchild --;       // Child exited
+                        memcpid[i]=0;
+                  }
               }
           }
-
-
       }
       else {             // I am a child
-          usleep(500);
+          usleep(5000000);
           exit(EXIT_SUCCESS);
       }
     }
+
+    return 0;
 }
